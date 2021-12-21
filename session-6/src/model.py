d5988392e30c06c76315ff9d3fb4f1879b6c8754
Helena Martín Hernández <helena.martin@alum.esci.upf.edu>
2021-12-21 19:47:03 +0000

    test

--- session-6/src/model.py
+++ session-6/src/model.py
@@ -5,8 +5,14 @@ import torch.nn as nn
 class RegressionModel(nn.Module):
     # You should build your model with at least 2 layers using tanh activation in between
     def __init__(self, input_size, hidden_size):
-        ...
+        super().__init__()
+        self.h1 = nn.Linear(input_size,hidden_size)
+        self.a1 = nn.Tanh()
+        self.h2 = nn.Linear(hidden_size,1)
+        self.a2 = nn.Tanh()
 
     def forward(self, x):
-        ...
+        h1 = self.a1(self.h1(x))
+        y = self.a2(self.h2(h1))
+        return y
         
--- session-6/src/train.py
+++ session-6/src/train.py
@@ -3,6 +3,8 @@ import time
 
 import pandas as pd
 import torch
+import torch.nn.functional as F
+import torch.optim as optim
 from torch.utils.data import DataLoader, TensorDataset
 from sklearn.model_selection import train_test_split
 
@@ -16,13 +18,13 @@ def train_epoch(dataloader, model, optimizer, criterion):
     # Train the model
     train_loss = 0
     for X, y in dataloader:
-        optimizer...
+        optimizer.zero_grad()
         X, y = X.to(device), y.to(device)
-        y_ = ...
-        loss = ...
+        y_ = model(X)
+        loss = criterion(y_,y)
         train_loss += loss.item() * len(y)
-        loss...
-        optimizer...
+        loss.backward()
+        optimizer.step()
 
     return train_loss / len(dataloader.dataset)
 
@@ -32,15 +34,15 @@ def test_epoch(dataloader: DataLoader, model, criterion):
     for X, y in dataloader:
         X, y = X.to(device), y.to(device)
         with torch.no_grad():
-            y_ = ...
-            loss = ...
+            y_ = model(X)
+            loss = criterion(y_,y)
             test_loss += loss.item() * len(y)
 
     return test_loss / len(dataloader.dataset)
 
 
 def load_data():
-    df = pd.read_csv("/data/housing.csv")
+    df = pd.read_csv("/tmp/data/housing.csv")
     train_df, test_df = train_test_split(df, test_size=0.2)
     train_X, train_y = train_df.drop(["ID", "MEDV"], axis=1), train_df["MEDV"]
     test_X, test_y = test_df.drop(["ID", "MEDV"], axis=1), test_df["MEDV"]
@@ -63,40 +65,42 @@ def train():
     # Now you should normalize your data (without Data Leakage) and wrap it in a Dataset
 
     # Start by converting the numpy tensors to torch tensors
-    train_X, train_y = ...
-    test_X, test_y = ...
+    train_X = torch.tensor(train_X, dtype=torch.float32)
+    train_y = torch.tensor(train_y, dtype=torch.float32).reshape(-1 ,1)
+    test_X = torch.tensor(test_X, dtype=torch.float32) 
+    test_y = torch.tensor(test_y, dtype=torch.float32).reshape(-1 ,1)
 
     # Compute the mean and std in the correct dimension
-    x_mean = ...
-    x_std = ...
-    y_mean = ...
-    y_std = ...
+    x_mean = torch.mean(train_X)
+    x_std = torch.std(train_X)
+    y_mean = torch.mean(train_y)
+    y_std = torch.std(train_y)
 
     # Apply it to our data ((data - mean)/std)
-    train_X = ...
-    train_y = ...
+    train_X = ((train_X - x_mean)/ x_std)
+    train_y = ((train_y - y_mean)/ y_std)
 
-    test_X = ...
-    test_y = ...
+    test_X = ((test_X - x_mean)/ x_std)
+    test_y = ((test_y - y_mean)/ y_std)
 
     # Instantiate the datasets
-    train_dataset = ...
-    test_dataset = ...
+    train_dataset = TensorDataset(train_X,train_y)
+    test_dataset = TensorDataset(test_X,test_y)
 
     # Instantiate data loaders
-    train_loader = ...
-    test_loader = ...
+    train_loader = DataLoader(train_dataset, batch_size=BATCH_SIZE)
+    test_loader = DataLoader(test_dataset, batch_size=BATCH_SIZE)
 
     input_size = train_X.shape[1]
 
     # Load the model
-    model = ...
+    model = RegressionModel(input_size,HIDDEN_SIZE)
         
     # You should use a loss function appropriate for regression
-    criterion = ...
+    criterion = F.mse_loss
 
     # Setup optimizer. SGD with lr=0.1 will work
-    optimizer = ...
+    optimizer = optim.SGD(model.parameters(), lr=0.1)
 
     for epoch in range(N_EPOCHS):
 
@@ -114,21 +118,21 @@ def train():
 
     # Now save the artifacts of the training
     # Do not change this path (unless debugging). You should mount a Docker volume to it
-    savedir = "/checkpoints/checkpoint.pt"
+    savedir = "/tmp/checkpoints/checkpoint.pt"
     print(f"Saving checkpoint to {savedir}...")
     # We can save everything we will need later in the checkpoint.
     # Here, we could save a feature transformer if we had used one
     checkpoint = {
-        "model_state_dict": ...,
-        "optimizer_state_dict": ...,
-        "input_size": ...,
-        "hidden_size": ...,
-        "x_mean": ...,
-        "y_mean": ...,
-        "x_std": ...,
-        "y_std": ...,
+        "model_state_dict": model.state_dict(),
+        "optimizer_state_dict": optimizer.state_dict(),
+        "input_size": input_size,
+        "hidden_size": HIDDEN_SIZE,
+        "x_mean": x_mean,
+        "y_mean": y_mean,
+        "x_std": x_std,
+        "y_std": y_std,
     }
-    torch.save(...)
+    torch.save(checkpoint, savedir)
 
 if __name__ == "__main__":
     train()
